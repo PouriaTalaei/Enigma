d1f5de0335c12daac922ba71ca46104f692d0556
PouriaTalaei <talaei.pouria06@gmmai.com>
2022-12-27 22:02:44 +0330

    Set PlugBoard

--- src/Enigma.java
+++ src/Enigma.java
@@ -29,4 +29,16 @@ public class Enigma {
             asciNumLast--;
         }
     }
+
+    public void PlugBoard(String plugBoardLine) {
+        String plugBoardStr = plugBoardLine.substring(0, plugBoardLine.length() - 1);
+        String plugBoardStrArray[] = plugBoardStr.split(", ");
+        for (int i = 0; i < plugBoardStrArray.length; i++) {
+            PlugBoard.put(plugBoardStrArray[i].charAt(0), plugBoardStrArray[i].charAt(1));
+        }
+        for (int i = 0; i < plugBoardStrArray.length; i++) {
+            PlugBoard.put(plugBoardStrArray[i].charAt(1), plugBoardStrArray[i].charAt(0));
+        }
+    }
+
 }
